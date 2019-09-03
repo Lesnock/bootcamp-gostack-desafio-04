400ab5f0a90b9911718bc9cf59f7cfe9f5375d9a
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2019-09-03 15:35:46 -0300

    Removed unnecessary components

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-// Components
-import Comment from './Comment'
-
-function PostHeader ({ name, date, avatar }) {
+// Header with avatar, name and date
+function Header ({ name, date, avatar }) {
     return (
         <div className="post-header">
             <img src={avatar} className="avatar" alt=""/>
@@ -15,16 +13,22 @@ function PostHeader ({ name, date, avatar }) {
     )
 }
 
-function PostContent ({ content }) {
-    return <div className="content">{content}</div>
+// Post Comment
+function Comment ({ comment }) {
+    return (
+        <div className="comment">
+            <img src={comment.author.avatar} alt="avatar"/>
+            <div className="content"><span className="name">{comment.author.name}</span> {comment.content}</div>
+        </div>
+    )
 }
 
 export default function Post ({ name, date, content, comments, avatar }) {
     return (
         <div className="post">
-            <PostHeader name={name} date={date} avatar={avatar}></PostHeader>
-            <PostContent content={content}></PostContent>
-
+            <Header name={name} date={date} avatar={avatar}></Header>
+            <div className="content">{content}</div>
+            
             {comments.map(comment => (
                 <Comment key={comment.id} comment={comment}></Comment>
             ))}
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -55,7 +55,26 @@ export default class PostList extends Component {
                         content: 'Que maaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes!'
                     }
                 ]
-            }
+            },
+            {
+                id: 3,
+                author: {
+                    name: 'Julio Alcantara',
+                    avatar: 'http://localhost:8080/images/avatar@2x.png'
+                },
+                date: '04 Jun 2019',
+                content: 'Pessoal, alguém sabe se a Rocketseat está contratando?',
+                comments: [
+                    {
+                        id: 1,
+                        author: {
+                            name: 'Diego Fernandes',
+                            avatar: 'http://localhost:8080/images/profile@2x.png',
+                        },
+                        content: 'A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)'
+                    }
+                ]
+            },
         ]
     }
 
